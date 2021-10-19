1b6ff215506f9a8c4fa6eabfedc410a26f1cb473
robin <kemmeri@uni-bremen.de>
2021-10-19 13:59:22 +0200

    init server script basic framework

---
+++ server.py
@@ -0,0 +1,48 @@
+import socket
+from _thread import *
+import sys
+
+server = "192.168.178.30"  # local address for now
+port = 5555
+
+# AF_INET for ipv4
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+# TODO: change to 4 players later
+s.listen(2)
+
+print("waiting for connection, server is running")
+
+def threaded_client(conn):
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)  # amount of information to retrieve
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("disconnected")
+                break
+            else:
+                print("received: ", reply)
+                print("sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+
+        except:
+            break
+
+
+while True:
+    conn, addr = s.accept()
+    print("connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn,))
+
+#34
