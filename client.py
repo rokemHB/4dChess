b6d6a5ea0cb2da50848b79695a03868b07b3ff70
robin <kemmeri@uni-bremen.de>
2021-10-19 21:09:24 +0200

    clients get updates from server - some multithread issues to fix

--- client.py
+++ client.py
@@ -42,12 +42,15 @@ class Player():
     def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
 
+
 def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+        str = str.split(",")
+        return int(str[0]), int(str[1])
+
 
 def make_pos(tup):
     return str(tup[0]) + "," + str(tup[1])
+
 
 def redrawWindow(win, player, player2):
     win.fill((255, 255, 255))
@@ -59,7 +62,7 @@ def redrawWindow(win, player, player2):
 def main():
     run = True
     n = Network()
-    startPos = read_pos(n.getPos()) # Player 1, 2, etc.
+    startPos = read_pos(n.getPos())  # Player 1, 2, etc.
     p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
     p2 = Player(0, 0, 100, 100, (0, 255, 0))
     clock = pygame.time.Clock()
--- server.py
+++ server.py
@@ -18,22 +18,40 @@ s.listen(2)
 
 print("waiting for connection, server is running")
 
-def threaded_client(conn):
-    conn.send(str.encode("connected"))
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+pos = [(0,0), (100,100)]
+
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)  # amount of information to retrieve
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
 
             if not data:
                 print("disconnected")
                 break
             else:
-                print("received: ", reply)
+                if player == 1:  # TODO: extend to 4 players
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("received: ", data)
                 print("sending: ", reply)
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
 
         except:
             break
@@ -41,11 +59,10 @@ def threaded_client(conn):
     print("lost connection")
     conn.close()
 
-
+currentPlayer = 0  # add connecting players
 while True:
     conn, addr = s.accept()
     print("connected to: ", addr)
 
-    start_new_thread(threaded_client, (conn,))
-
-#34
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
