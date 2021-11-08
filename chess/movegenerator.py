217cd6a8e7278328e4a4b55f335178eccab5bf3c
Robin <kemmeri@uni-bremen.de>
2021-11-08 14:28:35 +0100

    started movegen, need to refactor board class to piece orientated first

--- chess/movegenerator.py
+++ chess/movegenerator.py
@@ -3,16 +3,24 @@ from chess.move import Move
 
 class MoveGenerator:
     moves = []
+    board = None
+
+    def __init__(self, board):
+        self.board = board
 
     def generate_moves(self):
-        self.moves.append(Move(None, 75, 89))
-        return self.moves
+        # Flags
+        in_check = False
+        in_double_check = False
+        pins_exist_in_position = False
+        check_ray_bitmask = 0
+        pin_ray_bitmask = 0
 
+        # color to move ....
+        pass
 
+        # self.moves.append(Move(None, 75, 89))
+        # return self.moves
 
-
-
-
-
-
-
+    def generate_sliding_attack_map(self):
+        pass
